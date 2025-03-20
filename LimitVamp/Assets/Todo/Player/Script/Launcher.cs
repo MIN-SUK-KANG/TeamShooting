f13930d80c0810e28cb96155c0a09357a08118e7
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-03-21 03:28:42 +0900

    0320 팀 슈팅프로젝트 업데이트
    3/20일 업데이트내용
    총알 이제 나와서 총알이 가장 가까운 적에게 날아가는데 갈아가는도중 죽었어도 가는중이였던 총알은 그대로 쭉나감
    캐릭터가 총을 쏘면 한방에 죽었었는데 체력 시스템 도입하여서 총공격력 5이고 적체력 20이면 4번맞아야지 죽음
    적이 죽으면 아이템 뿌리는데 아이템 먹으면 현재 경험치, 레벨업필요경험치, 레벨 변수 생성
    
    3/21일 01:00 AM 업데이트 내용
    근거리 무기 생성함 일단 근거리 무기 10으로 생성해놨고 근거리로 한대때리고 원거리로 두대때리면 적이 죽는 거 확인함
    플레이어 체력시스템 만들었음 기본 100으로하고 닿아있는동안 빨간색이 되고 체력이 깎이는데 0이하로 떨어지면 캐릭터 제거 묘지생성
    아직 근거리 무기의 문제가 좀 있음 고쳐보도록 할꺼임(근거리 공격에 데미지를 받고 아이템까지먹어버림)

--- LimitVamp/Assets/Todo/Player/Script/Launcher.cs
+++ LimitVamp/Assets/Todo/Player/Script/Launcher.cs
@@ -4,9 +4,10 @@ public class Launcher : MonoBehaviour
 {
     public GameObject bullet; //총알 프리팹 담을 변수
     public Player player;
+    public float bullet_speed = 1.0f;
     void Start()
     {
-        InvokeRepeating("Shoot", 3f, 0.5f);
+        InvokeRepeating("Shoot", 3f, bullet_speed);//3초후 bullet_speed의 시간만큼 텀이있음
     }
 
     void Update()
@@ -17,7 +18,7 @@ public class Launcher : MonoBehaviour
     {
         if (player.nearestTarget != null)
         {
-            Instantiate(bullet, player.nearestTarget.position, Quaternion.identity);
+            Instantiate(bullet, transform.position, Quaternion.identity);//총알
             Debug.Log($"Bullet Spawn Position: {player.nearestTarget.position}");
         }
     }
